3b0f76c2f314730513960f6727f82e2b9145a352
Mateus Marchinack <167545286+Marchinack@users.noreply.github.com>
2024-09-02 12:19:49 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    int contador = 1;
+    while(contador < 11){
+
+      System.out.println("Bom dia!");
+      contador = contador + 1;
+    }
+  } 
+}
